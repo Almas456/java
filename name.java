a4907d448a7bc2896a33ee6dd12f095352f4f49a
Almas Sardar Shaikh <95541631+Almas456@users.noreply.github.com>
2025-02-14 16:46:15 +0530

    Update name.java

--- name.java
+++ name.java
@@ -5,7 +5,7 @@ public class Main {
     // year to be checked
     int year = 1900;
     boolean leap = false;
-
+    System.out.println("I am workning");
     // if the year is divided by 4
     if (year % 4 == 0) {
 
